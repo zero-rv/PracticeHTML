9373ef872a2f3f499b6aac04bd21187ad8d7cdeb
Zero <152014061+zero-rv@users.noreply.github.com>
2024-08-07 11:12:08 +0700

    Add files via upload

--- webproject/assets/js/script.js
+++ webproject/assets/js/script.js
@@ -4,9 +4,12 @@ const $$ = document.querySelectorAll.bind(document);
 //Hien thi va an mobile nav khi click nut list
 const open_menu = $('button.open-menu');
 open_menu.onclick = function () {
-    $('#mobile-header').style.width = "300px";
+    $('#mobile-header').style.width = '300px';
+    $('#main::after').style.display = 'block';
+    console.log($('#main::after'))
 }
 const close_menu = $('button.close-menu');
 close_menu.onclick = function () {
     $('#mobile-header').style.width = "0px";
+    $('#main::after').style.display = 'none';
 }
