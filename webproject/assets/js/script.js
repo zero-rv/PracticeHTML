deab49e2d32ed8ced21ac8491bbbc40aa28aecc3
Zero <152014061+zero-rv@users.noreply.github.com>
2024-08-07 17:17:09 +0700

    Add files via upload

--- webproject/assets/js/script.js
+++ webproject/assets/js/script.js
@@ -5,11 +5,10 @@ const $$ = document.querySelectorAll.bind(document);
 const open_menu = $('button.open-menu');
 open_menu.onclick = function () {
     $('#mobile-header').style.width = '300px';
-    $('#main::after').style.display = 'block';
-    console.log($('#main::after'))
+    $('.cover').classList.add('cover-bg');
 }
 const close_menu = $('button.close-menu');
 close_menu.onclick = function () {
     $('#mobile-header').style.width = "0px";
-    $('#main::after').style.display = 'none';
+    $('.cover.cover-bg').classList.remove('cover-bg');
 }
